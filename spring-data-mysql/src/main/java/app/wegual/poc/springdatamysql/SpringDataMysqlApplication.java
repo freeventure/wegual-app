cfd0eaf7361744cbb1049f3f1e3e0b619597de29
Suhas Mutatkar <smutatkar@gmail.com>
2019-09-06 10:18:35 -0700

    Added Spring data MySQL project

---
+++ spring-data-mysql/src/main/java/app/wegual/poc/springdatamysql/SpringDataMysqlApplication.java
@@ -0,0 +1,13 @@
+package app.wegual.poc.springdatamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataMysqlApplication.class, args);
+	}
+
+}
---
+++ spring-data-mysql/src/test/java/app/wegual/poc/springdatamysql/SpringDataMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package app.wegual.poc.springdatamysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataMysqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
