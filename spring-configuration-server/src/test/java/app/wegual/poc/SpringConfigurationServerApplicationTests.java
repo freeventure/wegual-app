339275ffe0fa32f6246035e0d35f74b97d27ca66
shruti9923 <shrutijain9923@gmail.com>
2020-01-29 11:44:45 +0530

    added configuration server

---
+++ spring-configuration-server/src/test/java/app/wegual/poc/SpringConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.wegual.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
