a9d3e9f0ebc68433201ad81d78b874a722bd1189
Suhas Mutatkar <smutatkar@gmail.com>
2020-04-18 22:41:49 -0700

    Adding abstract message object to remove compile issue for remote team.

---
+++ common-model/src/main/java/app/wegual/common/message/AbstractMessageObject.java
@@ -0,0 +1,26 @@
+package app.wegual.common.message;
+
+import app.wegual.common.model.ActionTarget;
+
+public abstract class AbstractMessageObject<T> implements ActionTarget<T> {
+	private static final long serialVersionUID = -2379467793470635779L;
+	protected T id;
+	protected String permalink;
+	protected String type;
+	private String summary;
+	private String name;
+	
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
